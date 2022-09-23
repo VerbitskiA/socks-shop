9000df665948463858c2647e9d6066dbae231629
VerbitskiA <eternity.2019@mail.ru>
2022-09-23 19:11:22 +0300

    init repo.

---
+++ ConsoleApp/Data/ShopContext.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext()
+        {
+           //Database.EnsureDeleted();
+            Database.EnsureCreated();            
+        }
+
+        public DbSet<Sock> Socks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=COMPUTER;Database=Shop;Trusted_Connection=True;");
+            //optionsBuilder.UseNpgsql("");
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp.Data;
+using ConsoleApp.Data.Entities;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShopContext context = new ShopContext();
+
+            var updatedSock = context.Socks.FirstOrDefault(x => x.Name == "Some name");
+
+            if (updatedSock is not null)
+            {
+                updatedSock.Name = "Крутые тёплые носки";
+                updatedSock.Price = 30;
+
+                context.Update(updatedSock);
+
+                context.SaveChanges();
+            }
+
+            List<Sock> result = context.Socks.ToList();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"Id: {item.Id} | Name:{item.Name} | Color: {item.Color} | Image: {item.ImageName} | Price: {item.Price}");
+            }
+        }       
+    }
+}
